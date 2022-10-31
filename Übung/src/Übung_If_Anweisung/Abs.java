be8382a5da6fc2dde7c2129a8a0c0ea68f3d9012
-27IQ <83514344+27IQ@users.noreply.github.com>
2022-10-31 22:23:48 +0100

    Übung if Anweisung final

---
+++ Übung/src/Übung_If_Anweisung/Abs.java
@@ -0,0 +1,22 @@
+package Übung_If_Anweisung;
+
+// Betragsfunktion
+// 
+// Implementieren Sie die Betragsfunktion in der Methode "abs". Die Lösung der Betragsfunktion f(x) = |x| ist x, wenn x positiv ist, und -x, wenn x negativ ist. 
+public class Abs {
+
+	public static int abs(int value) {
+		if(value<0){
+			value*=-1;
+		}
+		return value;
+	}
+
+	public static void main(String[] args) {
+		// Testaufruf
+		System.out.println("Der Betrag von -24 = " + abs(-24) + " (24 ist korrekt)");
+		System.out.println("Der Betrag von   0 = " + abs(0) + " (0 ist korrekt)");
+		System.out.println("Der Betrag von   4 = " + abs(4) + " (4 ist korrekt)");
+	}
+
+}
---
+++ Übung/src/Übung_If_Anweisung/Answer.java
@@ -0,0 +1,23 @@
+package Übung_If_Anweisung;
+
+
+public class Answer {
+	/*
+	 * Die main Methode verschiedene Aufrufe testen
+	 */
+	public static void main(String[] args) {
+		System.out.println(Answer.theAnswer(42));//sollte true zurueckgeben
+		System.out.println(Answer.theAnswer(27));//sollte false zurueckgeben
+	}
+	
+	/*
+	 * Diese Methode soll zurueckgeben, ob der �bergeben Parameter 42 ist.
+	 * Wenn dies der Fall ist, dann soll true zur�ckgegeben werden, ansonsten false.
+	 */
+	public static boolean theAnswer(int zahl)
+	{
+		//Hier beginnt der zu bearbeitende Bereich
+		return zahl==42;
+		//Hier endet der zu bearbeitende Bereich
+	}
+}
