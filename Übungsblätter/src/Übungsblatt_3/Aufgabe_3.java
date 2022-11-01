f7ef500e4a6f9895111630e420f3e4cdffd0a6b4
-27IQ <83514344+27IQ@users.noreply.github.com>
2022-11-01 02:36:09 +0100

    update nr 3

--- Übungsblätter/src/Übungsblatt_3/Aufgabe_3.java
+++ Übungsblätter/src/Übungsblatt_3/Aufgabe_3.java
@@ -3,8 +3,9 @@ package Übungsblatt_3;
 public class Aufgabe_3 {
     
     public static int[][] filterArray(int[][] array, int filter){
-        int offset;
+        int offset=0,offset2=0,empty=0;
         int[] lengths=new int[array.length];
+        boolean haswritten;
 
         for(int i=0;i<array.length;i++){
             lengths[i]=array[i].length;
@@ -18,20 +19,41 @@ public class Aufgabe_3 {
             }
         }
 
-        int[][] filteredArray=new int[lengths.length][];
+        for(int i=0;i<lengths.length;i++){
+            if(lengths[i]==0){
+                empty++;
+            }
+        }
+
+        int[] lengths2=new int[array.length-empty];
 
         for(int i=0;i<lengths.length;i++){
-            filteredArray[i]=new int[lengths[i]];
+            if(lengths[i]!=0){
+                lengths2[i-offset]=lengths[i];
+            }else{
+                offset++;
+            }
+        }
+
+        int[][] filteredArray=new int[lengths2.length][];
+
+        for(int i=0;i<lengths2.length;i++){
+            filteredArray[i]=new int[lengths2[i]];
         }
 
         for(int i=0;i<array.length;i++){
             offset=0;
+            haswritten=false;
             for(int a=0;a<array[i].length;a++){
                 if(array[i][a]!=filter){
-                    filteredArray[i][a-offset]=array[i][a]; 
+                    filteredArray[i-offset2][a-offset]=array[i][a]; 
+                    haswritten=true;
                 }else{
                     offset++;
                 }
+            }
+            if(haswritten==false){
+                offset2++;
             }
         }
         return filteredArray;
@@ -46,8 +68,8 @@ public class Aufgabe_3 {
             }
             System.out.println("----------");
         }
-
-        int[][] test2={{19,9,3,0,13},{6,7,9,4,5}};
+        System.out.println("%%%%%%%%%%%");
+        int[][] test2={{19,9,3,0,13},{9,9,9},{1,6,14,9}};
         erg=filterArray(test2, 9);
         for (int[] is : erg) {
             for(int fo :is) {
