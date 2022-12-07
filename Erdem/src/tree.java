d63c189f4cac2ef5d5184556d6bbbef64ef3cca4
-27IQ <83514344+27IQ@users.noreply.github.com>
2022-12-07 23:55:07 +0100

    Jupyter install

---
+++ Erdem/src/tree.java
@@ -0,0 +1,19 @@
+import ch.aplu.turtle.*;
+
+public class tree {
+    
+    public tree(){
+        Turtle t=new Turtle();
+
+
+    }
+
+    public void zeichenBaum(int laenge){
+        
+    }
+
+
+    public static void main(String[] args) {
+        new tree();
+    }
+}
